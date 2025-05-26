17baeae89f0515a617131a58455739b3abf00e6b
Tapan Tripathi <tapan.tripathi4@gmail.com>
2025-05-26 08:05:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package org.example;
+import
+@SpringbootApplication
+@RestControlller
+public class Main {
+    public static void main(String[] args) {
+        SpringApllication.run(main.class,args);
+    }
+    @GetMapping
+    Public List <Employee>
+    {
+        return List.of()
+    }
+}
